8696ce4f18549ab7ae0d026d6ce885ee3b78bd35
Alexandr Turdiyev <alexandr.turdiyev@gmail.com>
2024-07-07 14:35:00 +0530

    feat: add main.js script file and connect to index.html

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM загружен');
+});
+
+window.addEventListener('load', function() {
+    console.log('Все ресурсы загружены');
+});
+
